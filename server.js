3735ff0da9f546d8da8cad38245c8addcfaec265
levink <levinkostya@yandex.ru>
2026-02-01 21:47:45 +0300

    Works index.html + styles.css

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const http = require('http');
-const host = '127.0.0.1';
-const port = 1221;
+const fs = require("fs");
 
+
+const host = '127.0.0.1';
+const port = 3500;
 
 process.on('SIGINT', () => { 
    console.log('\nSIGINT received: closing server');
@@ -11,13 +13,23 @@ process.on('SIGINT', () => {
     });
 });
 
-const server = http.createServer(function(req, res) {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.write('Hello World!');
-    res.end();
+const server = http.createServer(function (req, res) {
+    
+    const filePath = 'site/' + req.url.substring(1);
+    fs.access(filePath, fs.constants.R_OK, err => {
+        if (err) {
+            res.statusCode = 404;
+            res.end("Not found.");
+        }
+        else {
+            res.statusCode = 200;
+            fs.createReadStream(filePath).pipe(res);
+        }
+    });
 });
 
 server.listen(port, host, () => {
     console.log('Server running...');
 });
+
+
