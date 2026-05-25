2e85126942bc448c7118d198666549f178b5f612
levink <levinkostya@yandex.ru>
2026-05-25 19:58:25 +0300

    correct settings path

--- public/settings.php
+++ public/settings.php
@@ -1,6 +1,7 @@
 <?php
-    $filePath = realpath(getenv('PROFI_SETTINGS'));
+    $filePath = realpath($_SERVER["DOCUMENT_ROOT"] . "/" . getenv('PROFI_SETTINGS'));
     $settings = parse_ini_file($filePath, true);
     define('SETTINGS', $settings);
     unset($settings);
+    unset($filePath);
 ?>
